b22902f4c8bbd7a150896b043691de4907db6588
Lance Bailey <lancebailey222@gmail.com>
2021-08-16 12:36:31 -0400

    heroku

--- models/index.js
+++ models/index.js
@@ -3,14 +3,14 @@ const Exercise = require('./Exercise');
 const User = require('./User');
 const Routine = require('./Routine');
 
-
+Exercise.belongsTo(Category, {
+    foreignKey: "category_id"
+});
 
 Category.hasMany(Exercise, {
     foreignKey: "category_id"
 });
-Exercise.belongsTo(Category, {
-    foreignKey: "category_id"
-});
+
 Routine.belongsTo(User, {
     foreignKey: "creator_id"
 });
